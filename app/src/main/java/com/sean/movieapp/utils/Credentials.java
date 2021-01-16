91e2e38ea6fd1b2d8af9d3f5f3155411e3c4bb90
sean <sean.ts.lim@tiongnam.com.my>
2021-01-16 12:42:03 +0800

    credentials and retrofit

---
+++ app/src/main/java/com/sean/movieapp/utils/Credentials.java
@@ -0,0 +1,8 @@
+package com.sean.movieapp.utils;
+
+public class Credentials {
+
+    public static final String BASE_URL = "https://api.themoviedb.org";
+
+    public static final String API_KEY = "45718f0c6912aeb42897129e413d9b3b";
+}
